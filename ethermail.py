99f7efde0cacb01a2159037fcb7ed924e1f0b469
Flamingo-at <119711235+Flamingo-at@users.noreply.github.com>
2023-02-27 18:06:00 +0200

    Add files via upload

--- ethermail.py
+++ ethermail.py
@@ -9,37 +9,7 @@ from eth_account.messages import encode_defunct
 from pyuseragents import random as random_useragent
 
 
-async def sending_captcha(client: ClientSession):
-    try:
-        response = await client.get(f'http://api.captcha.guru/in.php?key={user_key}&method=userrecaptcha \
-            &googlekey=6LdmCaMhAAAAAHuCRyI8Y_K3JbITDhW623QkEPIi&pageurl=https://ethermail.io/')
-        data = await response.text()
-        if 'ERROR' in data:
-            logger.error(print(data))
-            return(await sending_captcha(client))
-        id = data[3:]
-        return(await solving_captcha(client, id))
-    except:
-        raise Exception()
-
-
-async def solving_captcha(client: ClientSession, id: str):
-    for i in range(100):
-        try:
-            response = await client.get(f'http://api.captcha.guru/res.php?key={user_key}&action=get&id={id}')
-            data = await response.text()
-            if 'ERROR' in data:
-                logger.error(print(data))
-                raise Exception()
-            elif 'OK' in data:
-                return(data[3:])
-            return(await solving_captcha(client, id))
-        except:
-            raise Exception()
-    raise Exception()
-
-
-async def create_email(client: ClientSession):
+async def create_email(client: ClientSession) -> str:
     try:
         response = await client.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1")
         email = (await response.json())[0]
@@ -50,86 +20,73 @@ async def create_email(client: ClientSession):
         return await create_email(client)
 
 
-async def create_wallet():
+async def create_wallet() -> tuple[str, str]:
     account = w3.eth.account.create()
-    return(str(account.address), str(account.privateKey.hex()))
+    return str(account.address), str(account.privateKey.hex())
 
 
-async def create_signature(private_key: str):
+async def create_signature(private_key: str) -> str:
     message = encode_defunct(
         text='I have read and accept the terms and conditions of the website.')
     signed_message = w3.eth.account.sign_message(message, private_key)
-    return(signed_message.signature.hex())
+    return signed_message.signature.hex()
 
 
-async def get_token(client: ClientSession, useragent: str):
+async def get_token(client: ClientSession) -> str:
     try:
-        response = await client.get(f'https://ethermail.io/?afid={ref}',
-                                    headers={
-                                        'user-agent': useragent
-                                    })
-        data = await response.text()
-        return(findall(r'data-csrf-token="(.+)"', data)[0])
+        response = await client.get(f'https://ethermail.io/?afid={ref}')
+        return findall(r'xAccessToken:"(.+)"', await response.text())[0]
     except:
-        raise Exception()
+        raise
 
 
-async def register(client: ClientSession, address: str, private_key: str, csrf_token: str, useragent: str):
+async def register(client: ClientSession, address: str, private_key: str):
     try:
-        response = await client.post('https://ethermail.io/account/login',
+        response = await client.post('https://ethermail.io/frontend-api/account/authenticate',
                                      json={
                                          "message": "I have read and accept the terms and conditions of the website.",
                                          "web3Address": address,
-                                         'recaptchaResponse': await sending_captcha(client),
                                          "signature": await create_signature(private_key),
-                                         "remember": 'true',
-                                         "_csrf": csrf_token
-                                     }, headers={
-                                         'referer': f'https://ethermail.io/?afid={ref}',
-                                         'user-agent': useragent
+                                         "scope": 'master',
+                                         "token": True
                                      })
-        data = await response.text()
-        if '404 Error' in data:
-            raise Exception()
+        data = (await response.json())['success']
+        if not data:
+            raise
     except:
-        raise Exception()
-
-
-async def add_email(client: ClientSession, email: str, useragent: str):
-    try:
-        await client.post('https://ethermail.io/api/users/onboarding',
-                          json={
-                              "email": email
-                          }, headers={
-                              'referer': f'https://ethermail.io/?afid={ref}',
-                              'user-agent': useragent
-                          })
-    except:
-        raise Exception()
+        raise
 
 
 async def worker():
     while True:
         try:
-            async with aiohttp.ClientSession() as client:
+            async with aiohttp.ClientSession(
+                headers={"user-agent": random_useragent()}
+            ) as client:
 
                 address, private_key = await create_wallet()
-                useragent = random_useragent()
                 email = await create_email(client)
 
-                logger.info('Get token')
-                csrf_token = await get_token(client, useragent)
+                logger.info("Get token")
+                access_token = await get_token(client)
+
+                client.headers.update({
+                    'cookie': f'afid={ref}',
+                    'referer': f'https://ethermail.io/?afid={ref}',
+                    'x-access-token': access_token
+                })
 
                 logger.info('Registration')
-                await register(client, address.lower(), private_key, csrf_token, useragent)
+                await register(client, address.lower(), private_key)
 
                 logger.info('Add email')
-                await add_email(client, email, useragent)
-
+                await client.post('https://ethermail.io/api/users/onboarding',
+                                  json={
+                                      "email": email
+                                  })
         except:
             logger.error('Error\n')
         else:
-            logger.info('Saving data')
             with open('registered.txt', 'a', encoding='utf-8') as file:
                 file.write(f'{email}:{address}:{private_key}\n')
             logger.success('Successfully\n')
@@ -143,11 +100,9 @@ async def main():
 
 
 if __name__ == '__main__':
-
     print("Bot Ethermail @flamingoat\n")
 
     ref = input('Referral code: ')
-    user_key = input('Captcha key: ')
     delay = int(input('Delay(sec): '))
     threads = int(input('Threads: '))
 
